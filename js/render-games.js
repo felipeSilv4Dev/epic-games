a1c2514804c5e297f1f7a41e916d03fcd0504037
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-04-02 10:40:42 -0300

    adicioando título e point a games html

--- js/render-games.js
+++ js/render-games.js
@@ -1,99 +1,103 @@
 export default function renderGamer() {}
-
 const creatHome = (games) => {
+  const html = document.querySelector("html title");
   const home = document.querySelector(".jogo");
-  const homeH1 = document.querySelector(".jogo-h1 h1");
   const body = document.querySelector("body");
+  const homeH1 = document.querySelector(".jogo-h1");
   const gameId = +window.location.href.split("=")[1];
 
   const game = games.find((item) => item.id === gameId);
 
   if (game) {
+    html.innerText = game.title;
     body.className = game.class;
-    homeH1.innerHTML = /*html*/ game.title;
+    homeH1.innerHTML = /*html*/ `	
+		<h1>${game.title}</h1>
+		<span class="jogo-span-intro">${game.point}</span>
+		`;
     home.innerHTML = /*html*/ `
-			<div>
-							<video
-								src="../${game.srcVideo}"
-								muted
-								poster="../${game.srcName}"
-								autoplay
-								loop
-							>
-								<source src="../${game.srcVideo}" />
-							</video>
-							<div class="jogo-texto flex">
-								<img
-									src="../${game.srcName}"
-									width="348"
-									height="196"
-									alt="jogo ${game.title}"
-								/>
-								<div>
-									<h2>${game.title}</h2>
-									<p>Trailer</p>
+		<div>
+		<video
+		src="../${game.srcVideo}"
+		muted
+		poster="../${game.srcName}"
+		autoplay
+		loop
+		>
+		<source src="../${game.srcVideo}" />
+		</video>
+		<div class="jogo-texto flex">
+		<img
+		src="../${game.srcName}"
+		width="348"
+		height="196"
+		alt="jogo ${game.title}"
+		/>
+		<div>
+		<h2>${game.title}</h2>
+		<p>Trailer</p>
+		</div>
+		</div>
 								</div>
-							</div>
-						</div>
-		
-						<div class="jogo-info">
-							<div>
+								
+								<div class="jogo-info">
+								<div>
 								<img
-									class="nome-jogo"
+								class="nome-jogo"
 									src="../${game.srcGif}"
 									width="1056"
 									height="594"
 									alt="jogo ${game.title}"
-								/>
-							</div>
-		
-							<div class="jogo-info-detalhe">
-								<span class="jogo-span-intro">Jogo Base</span>
-								<div class="detalhe-span flex">
+									/>
+									</div>
+									
+									<div class="jogo-info-detalhe">
+									<span class="jogo-span-intro">Jogo Base</span>
+									<div class="detalhe-span flex">
 									<span>-${game.percentage}%</span>
 									<span class="preco-antigo">₹${game.oldPrice}</span>
 									<span>₹${game.newPrice}</span>
-								</div>
-		
-								<div class="detalhe-btn">
+									</div>
+									
+									<div class="detalhe-btn">
 									<a href="#Baixe">Baixe Agora</a>
-									<a href="#Baixe">Adicionar ao Carrinho</a>
-									<a href="#Baixe" class="btn-detalhe-list flex"
-										><img
-											src="../img/produtos/mais.svg"
-											width="24"
-											height="25"
-											alt="mais"
-										/>
-										Adicionar a Lista</a
-									>
-								</div>
-		
-								<div>
-									<div class="detalhe-extra flex">
-										<p>Empresa</p>
-										<p>${game.company}</p>
-									</div>
-									<div class="detalhe-extra flex">
-										<p>Desenvolvedor</p>
-										<p>${game.company}</p>
-									</div>
-									<div class="detalhe-extra flex">
-										<p>Data de lançamento</p>
-										<p>${game.date}</p>
-									</div>
-									<div class="detalhe-extra flex">
-										<p>Plataforma</p>
-										<img
-											src="../img/produtos/logo-w.svg"
-											width="24"
-											height="25"
-											alt="logo"
-										/>
-									</div>
-								</div>
+							<a href="#Baixe">Adicionar ao Carrinho</a>
+							<a href="#Baixe" class="btn-detalhe-list flex"
+							><img
+							src="../img/produtos/mais.svg"
+							width="24"
+							height="25"
+							alt="mais"
+							/>
+							Adicionar a Lista</a
+							>
 							</div>
-						</div>`;
+							
+							<div>
+							<div class="detalhe-extra flex">
+								<p>Empresa</p>
+								<p>${game.company}</p>
+								</div>
+								<div class="detalhe-extra flex">
+								<p>Desenvolvedor</p>
+								<p>${game.company}</p>
+								</div>
+								<div class="detalhe-extra flex">
+								<p>Data de lançamento</p>
+								<p>${game.date}</p>
+								</div>
+								<div class="detalhe-extra flex">
+								<p>Plataforma</p>
+								<img
+								src="../img/produtos/logo-w.svg"
+								width="24"
+								height="25"
+								alt="logo"
+								/>
+								</div>
+								</div>
+								</div>
+								</div>`;
   }
 };
 

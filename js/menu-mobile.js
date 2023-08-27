26094ebaa6295e7107a47f357173072530a4f086
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-08-27 14:04:34 -0300

    refatorando menu mobile

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,30 +1,42 @@
-const btn = document.querySelector(".menu__mobile-bg");
-const btnBg = document.querySelector(".mobile__bg");
-const body = document.querySelector("body");
-
-const activeMenu = function (e) {
-  btn.classList.toggle("active");
-  btnBg.classList.toggle("active");
-  body.classList.toggle("active");
-};
-
-const removeActive = function () {
-  btn.classList.remove("active");
-  btnBg.classList.remove("active");
-  body.classList.remove("active");
-};
-
-btn.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("menu__mobile") ||
-    e.target.classList.contains("menu__mobile-bg")
-  ) {
-    activeMenu(e);
+export default class MenuMobile {
+  constructor(btn, btnBg) {
+    this.btn = document.querySelector(btn);
+    this.btnBg = document.querySelector(btnBg);
+    this.body = document.querySelector("body");
+    this.active = "active";
   }
-});
-
-btnBg.addEventListener("click", (e) => {
-  if (e.target.classList.contains("mobile__bg")) {
-    removeActive();
+  activeMenu() {
+    this.scrollPage();
+    this.btn.classList.toggle(this.active);
+    this.btnBg.classList.toggle(this.active);
+    this.body.classList.toggle(this.active);
   }
-});
+
+  scrollPage() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  removeActive() {
+    this.btn.classList.remove(this.active);
+    this.btnBg.classList.remove(this.active);
+    this.body.classList.remove(this.active);
+  }
+
+  addEvents() {
+    this.btn.addEventListener("click", this.activeMenu);
+    this.btnBg.addEventListener("click", this.removeActive);
+  }
+
+  bind() {
+    this.activeMenu = this.activeMenu.bind(this);
+    this.removeActive = this.removeActive.bind(this);
+  }
+
+  init() {
+    this.bind();
+    this.addEvents();
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 import Slide from "./slide.js";
+import MenuMobile from "./menu-mobile.js";
+
 Slide();
+
+const menu = new MenuMobile(".menu__mobile-bg", ".mobile__bg");
+menu.init();

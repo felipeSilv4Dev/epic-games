28c2d38f4ef078e542eb4772388e715457c5e53c
felipeSilv4Dev <felipesilva.developer@gmail.com>
2024-01-06 22:54:33 -0300

    update header

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,47 +1,47 @@
-export default class MenuMobile {
-  constructor(btn, btnBg) {
-    this.btn = document.querySelector(btn);
-    this.btnBg = document.querySelector(btnBg);
+// export default class MenuMobile {
+//   constructor(btn, btnBg) {
+//     this.btn = document.querySelector(btn);
+//     this.btnBg = document.querySelector(btnBg);
 
-    this.nameclass = this.btnBg.className;
-    this.active = "active";
-  }
-  activeMenu() {
-    this.scrollPage();
+//     this.nameclass = this.btnBg.className;
+//     this.active = "active";
+//   }
+//   activeMenu() {
+//     this.scrollPage();
 
-    this.btn.classList.toggle(this.active);
-    this.btnBg.classList.toggle(this.active);
-    document.body.style.overflow = "hidden";
-  }
+//     this.btn.classList.toggle(this.active);
+//     this.btnBg.classList.toggle(this.active);
+//     document.body.style.overflow = "hidden";
+//   }
 
-  scrollPage() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  }
+//   scrollPage() {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   }
 
-  removeActive(e) {
-    if (e.target.classList.contains(this.nameclass)) {
-      this.btn.classList.remove(this.active);
-      this.btnBg.classList.remove(this.active);
-      this.body.classList.remove(this.active);
-      document.body.style.overflow = "auto";
-    }
-  }
+//   removeActive(e) {
+//     if (e.target.classList.contains(this.nameclass)) {
+//       this.btn.classList.remove(this.active);
+//       this.btnBg.classList.remove(this.active);
+//       this.body.classList.remove(this.active);
+//       document.body.style.overflow = "auto";
+//     }
+//   }
 
-  addEvents() {
-    this.btn.addEventListener("click", this.activeMenu);
-    this.btnBg.addEventListener("click", this.removeActive);
-  }
+//   addEvents() {
+//     this.btn.addEventListener("click", this.activeMenu);
+//     this.btnBg.addEventListener("click", this.removeActive);
+//   }
 
-  bind() {
-    this.activeMenu = this.activeMenu.bind(this);
-    this.removeActive = this.removeActive.bind(this);
-  }
+//   bind() {
+//     this.activeMenu = this.activeMenu.bind(this);
+//     this.removeActive = this.removeActive.bind(this);
+//   }
 
-  init() {
-    this.bind();
-    this.addEvents();
-  }
-}
+//   init() {
+//     this.bind();
+//     this.addEvents();
+//   }
+// }
